965fe94686f6d4cb1aac2e02342aaee04a589403
Martin Holst Swende <martin@swende.se>
2021-08-24 15:00:10 +0800

    core/vm: faster code analysis
    * core/vm: more detailed benchmark for jumpdest analysis
    
    * core/vm: make jd analysis benchmark alloc free
    
    * core/vm: improve jumpdest analysis
    
    * core/vm: improve worst-case
    
    * core/vm: further improvements in analysis
    
    * core/vm: improve jumpdest analysis >PUSH15
    
    * core/vm: make jd analysis ref by value
    
    * core/vm: fix misspell
    
    * core/vm: improve set8 and set16 a bit
    
    * core/vm: reduce amount of code
    
    * core/vm: optimize byte copying

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -701,6 +701,7 @@ func opCall(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) ([]by
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
+		ret = common.CopyBytes(ret)
 		callContext.memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
 	}
 	callContext.contract.Gas += returnGas
@@ -735,6 +736,7 @@ func opCallCode(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) (
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
+		ret = common.CopyBytes(ret)
 		callContext.memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
 	}
 	callContext.contract.Gas += returnGas
@@ -762,6 +764,7 @@ func opDelegateCall(pc *uint64, interpreter *EVMInterpreter, callContext *callCt
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
+		ret = common.CopyBytes(ret)
 		callContext.memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
 	}
 	callContext.contract.Gas += returnGas
@@ -789,6 +792,7 @@ func opStaticCall(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx)
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
+		ret = common.CopyBytes(ret)
 		callContext.memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
 	}
 	callContext.contract.Gas += returnGas
