57b8b25ee7fddfebe35b71428366fc86c95261cb
noam-alchemy <76969113+noam-alchemy@users.noreply.github.com>
2021-10-12 04:46:04 -0400

    eth,rpc: allow for flag configured timeouts for eth_call (#23645)
    * eth,rpc: allow for flag configured timeouts for eth_call
    
    * lint: account for package-local import order
    
    * cr: rename `rpc.calltimeout` to `rpc.evmtimeout`

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -148,6 +148,7 @@ var AppHelpFlagGroups = []flags.FlagGroup{
 			utils.GraphQLCORSDomainFlag,
 			utils.GraphQLVirtualHostsFlag,
 			utils.RPCGlobalGasCapFlag,
+			utils.RPCGlobalEVMTimeoutFlag,
 			utils.RPCGlobalTxFeeCapFlag,
 			utils.AllowUnprotectedTxs,
 			utils.JSpathFlag,
--- eth/api_backend.go
+++ eth/api_backend.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts"
@@ -338,6 +339,10 @@ func (b *EthAPIBackend) UnprotectedAllowed() bool {
 
 func (b *EthAPIBackend) RPCGasCap() uint64 {
 	return b.eth.config.RPCGasCap
+}
+
+func (b *EthAPIBackend) RPCEVMTimeout() time.Duration {
+	return b.eth.config.RPCEVMTimeout
 }
 
 func (b *EthAPIBackend) RPCTxFeeCap() float64 {
